297b188e766f25cf7b41a2f996a35ca6c6fb4af7
Maria Angela Pellegrino <marpel@isis.lab>
2018-12-13 12:27:51 +0100

    Added ZIP code, phone, mobile phone, and address checks - both evaluation functions and related tests

--- src/PrivacyConfigFactory.mjs
+++ src/PrivacyConfigFactory.mjs
@@ -27,6 +27,11 @@ const PRDATATYPES = {
     DT_UNKNOWN: { name: "UNKNOWN" },
     DT_EMAIL:   { name: "EMAIL" },
     DT_CF:      { name: "CF" },
+
+    DT_ZIPCODE : { name: "ZIPCODE"},
+    DT_MOBILEPHONE : { name: "MOBILE_PHONE"},
+    DT_PHONE : { name: "PHONE"},
+    DT_ADDRESS : {name: "ADDRESS"}
 };
 
 PRDATATYPES.DT_CF.evaluate = function (value) {
@@ -49,6 +54,50 @@ PRDATATYPES.DT_EMAIL.evaluate = function (value) {
     return { datatype: PRDATATYPES.DT_UNKNOWN, value: value };
 };
 
+PRDATATYPES.DT_ZIPCODE.evaluate = function(value) {
+    var regex = /^([0-9]{5})$/;
+
+    if (regex.test(value))
+        return { datatype: PRDATATYPES.DT_ZIPCODE, value: value };
+
+    return { datatype: PRDATATYPES.DT_UNKNOWN, value: value };    
+};
+
+PRDATATYPES.DT_MOBILEPHONE.evaluate = function(value) {
+    var regex = /^\((([+]|00)39)\)|(([+]|00)39)?\s?((313)|(32[034789])|(33[013456789])|(34[02456789])|(36[0368])|(37[037])|(38[0389])|(39[0123]))[\s-]?([\d]{7})$/;
+
+    value = value.replace(/-/gm, '');
+    value = value.replace(/\s/g,'');
+
+    if (regex.test(value))
+        return { datatype: PRDATATYPES.DT_MOBILEPHONE, value: value };
+
+    return { datatype: PRDATATYPES.DT_UNKNOWN, value: value };
+};
+
+PRDATATYPES.DT_PHONE.evaluate = function(value) {
+    var regex = /^(\((([+]|00)39)\)|(([+]|00)39))?\s?0\d{11}|\d{10}|\d{9}|\d{8}$/;
+
+    value = value.replace(/-/gm, '');
+    value = value.replace(/\s/g,'');
+
+    if (regex.test(value))
+        return { datatype: PRDATATYPES.DT_PHONE, value: value };
+
+    return { datatype: PRDATATYPES.DT_UNKNOWN, value: value };
+};
+
+PRDATATYPES.DT_ADDRESS.evaluate = function (value) {
+    var regex = /^via|viale|vico|corso|c.so|piazza|piazzetta|p.zza\s([a-z]+\s?)+[,|°]?\d*/i;
+
+    value = value.toLowerCase();
+
+    if (regex.test(value))
+        return { datatype: PRDATATYPES.DT_ADDRESS, value: value };
+
+    return { datatype: PRDATATYPES.DT_UNKNOWN, value: value };
+};
+
 PRDATATYPES.DT_UNKNOWN.evaluate = function (value) {
     return { datatype: PRDATATYPES.DT_UNKNOWN, value: value };
 };
@@ -66,7 +115,7 @@ export class PrivacyConfigFactory {
     }
 
     get types() {
-        return [ PRDATATYPES.DT_EMAIL, PRDATATYPES.DT_CF, PRDATATYPES.DT_UNKNOWN ];
+        return [ PRDATATYPES.DT_EMAIL, PRDATATYPES.DT_CF, PRDATATYPES.DT_ZIPCODE, PRDATATYPES.DT_MOBILEPHONE, PRDATATYPES.DT_PHONE, PRDATATYPES.DT_ADDRESS, PRDATATYPES.DT_UNKNOWN];
     }
 
     /*
--- tests/privacychecker/TestPrivacyCheckerDataset01.mjs
+++ tests/privacychecker/TestPrivacyCheckerDataset01.mjs
@@ -24,12 +24,20 @@ function runTests(datum) {
         const records = jsonDataset.records;
         const fieldKeys = jsonDataset.fields;
 
+        debugger
+
         let evaLogs = datachecker.evaluate(jsonDataset.records, fieldKeys);
 
         let viewBuilder = new PrivacyReportViewBuilder();
         let reportView = viewBuilder.build(evaLogs);
 
         assert.ok(reportView.DATATYPES["EMAIL"].warnings == 145, "Checking of emails");
+        assert.ok(reportView.DATATYPES["ZIPCODE"].warnings == 57, "Checking of ZIP codes");
+        assert.ok(reportView.DATATYPES["MOBILE_PHONE"].warnings == 45, "Checking of mobile phones");
+        assert.ok(reportView.DATATYPES["PHONE"].warnings == 16, "Checking of phone numbers");
+        //to check
+        assert.ok(reportView.DATATYPES["ADDRESS"].warnings == 57, "Checking of addresses");
+
     });//EndFunction.
 
 };//EndTests.
